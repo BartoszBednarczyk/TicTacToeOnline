d2612ef3c91496a6bb96cc55e8ad1f31bb4f2fa0
BartoszBednarczyk <bfbednarczyk@gmail.com>
2020-05-03 19:52:53 +0200

    1.0

---
+++ src/script.js
@@ -0,0 +1,192 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCftNLJX-1KzMg8cVtwzqp8kYI8KUPxSHU",
+    authDomain: "tictactoe-55d7e.firebaseapp.com",
+    databaseURL: "https://tictactoe-55d7e.firebaseio.com",
+    projectId: "tictactoe-55d7e",
+    storageBucket: "tictactoe-55d7e.appspot.com",
+    messagingSenderId: "81145643514",
+    appId: "1:81145643514:web:88746559dde8e379c46443"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var databaseListener;
+// databaseListener.on('value', function(snapshot){
+//     console.log("Zmieniło się coś");
+// })
+var roomNumber = 0;
+var yourSign = 0;
+var currentTurn = 0;
+var board = 0;
+
+
+
+function createGame(){
+    document.getElementById("menu").style.display = 'none';
+    document.getElementById("createGame").style.display = 'block';
+
+    //Check first empty room and set the game
+
+    database.ref().once('value').then(function(snapshot){
+        number = snapshot.numChildren();
+        var i = 1;
+        while(snapshot.child(i).exists()){
+            i++;
+        }
+        roomNumber = i;
+        database.ref(roomNumber).set({
+            "players": "1",
+            "turn": "X",
+            "board": [0,0,0,0,0,0,0,0,0]
+        });
+        document.getElementById("roomID").innerHTML = roomNumber;
+
+        console.log(roomNumber);
+
+        //Listener waiting for 2 players
+        database.ref(roomNumber).on('value', function playerListener(snapshot){
+            console.log("change");
+            if(snapshot.child("players").val() == "2"){
+            database.ref(roomNumber).off();
+            yourSign = "X";
+            game();
+            }
+        }); //; added here
+    });
+    
+
+}
+
+function backToMenuFromCreating(){
+    document.getElementById("menu").style.display = 'block';
+    document.getElementById("createGame").style.display = 'none';
+    document.getElementById("roomID").innerHTML = "Waiting for room...";
+    
+    //Check if room has been created and delete it
+
+    if (roomNumber != 0){
+        database.ref(roomNumber).once('value').then(function(snapshot){
+            if (snapshot.exists()){
+                database.ref(roomNumber).remove();
+                roomNumber = 0;
+            }
+        });
+    } 
+}
+
+function backToMenuFromJoining(){
+    document.getElementById("menu").style.display = 'block';
+    document.getElementById("joinGame").style.display = 'none';
+}
+
+
+function joinGame(){
+    document.getElementById("menu").style.display = 'none';
+    document.getElementById("joinGame").style.display = 'block';
+
+    yourSign = "O";
+}
+
+function checkID(){
+    var inputValue = 0;
+    inputValue = document.getElementById("inputCode").value;
+    if (inputValue == ""){inputValue=0}
+    database.ref().once('value').then(function(snapshot){
+    if(snapshot.child(inputValue).exists())
+    {
+        if(snapshot.child(inputValue+"/players").val()=="1"){
+            yourSign = "O";
+            database.ref(inputValue).update({
+                "players": "2"
+            });
+            roomNumber = inputValue;
+            game();
+        }
+        else {
+            alert("There's already 2 players!");
+        }
+    }
+    else{
+        alert("This room doesn't exist");
+    }
+    });
+}
+
+function game(){
+    document.getElementById("createGame").style.display = 'none';
+    document.getElementById("joinGame").style.display = 'none';
+    document.getElementById("game").style.display = 'grid';
+
+    database.ref(roomNumber).update({
+        "board": [0,0,0,0,0,0,0,0,0]
+    });
+    if (yourSign == "X"){
+        document.getElementById("yourSign").innerHTML = "❌";
+    }
+    else if (yourSign == "O"){
+        document.getElementById("yourSign").innerHTML = "⭕️";
+    }
+    else{
+        alert("Something went wrong. Please refresh the page and try again!");
+    }
+
+    database.ref(roomNumber).on('value', function (snapshot){
+        currentTurn = snapshot.child("turn").val();
+        board = snapshot.child("board").val();
+        console.log("board: " + board + "board[0]: " + board[0]);
+
+        console.log("Turn change");
+        console.log(roomNumber);
+        switch (currentTurn)
+        {
+            case "X":
+                document.getElementById("turn").innerHTML = "❌";
+                break;
+            case "O":
+                document.getElementById("turn").innerHTML = "⭕️";
+                break;
+        }
+        
+        for (var i = 0; i < 9; i++){
+            switch (board[i]){
+                case "0":
+                    break;
+                case "X":
+                    document.getElementById("btn"+i).innerHTML = "❌";
+                    break;
+                case "O":
+                    document.getElementById("btn"+i).innerHTML = "⭕️";
+                    break;
+            }
+        }
+    });
+}
+
+function pressButton(num){
+    if (yourSign == currentTurn){
+        if(board[num] == 0)
+        {
+            board[num] = yourSign;
+            database.ref(roomNumber).update({"board": board});
+            switch (currentTurn)
+            {
+                case "X":
+                    currentTurn = "O";
+                    database.ref(roomNumber).update({"turn": "O"});
+                    break;
+                case "O":
+                    currentTurn = "X";
+                    database.ref(roomNumber).update({"turn": "X"});
+                    break;
+            }
+        }
+        else{
+            alert("This place is already taken!");
+        }
+    }
+    else {
+        alert("Not your turn!")
+    }
+}
